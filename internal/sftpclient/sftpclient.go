d465b20237475235597de28e32f2a46eb908301a
Anton Arapov <arapov@gmail.com>
2019-03-07 11:08:26 +0100

    Use knownhosts subpackage for HostKeyVerification
    ssia
    
    Signed-off-by: Anton Arapov <arapov@gmail.com>

--- internal/sftpclient/sftpclient.go
+++ internal/sftpclient/sftpclient.go
@@ -1,19 +1,18 @@
 package sftpclient
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/pkg/sftp"
 	"github.com/spf13/viper"
 	"golang.org/x/crypto/ssh"
+	kh "golang.org/x/crypto/ssh/knownhosts"
 )
 
 type Info struct {
@@ -28,8 +27,6 @@ type Client struct {
 
 // NewClient creates a new SFTP client
 func (c *Info) NewClient() *Client {
-	hostKey := getHostKey(c.HostName)
-
 	key, err := ioutil.ReadFile(c.SSHKey)
 	if err != nil {
 		log.Fatalf("unable to read private key: %v", err)
@@ -41,14 +38,17 @@ func (c *Info) NewClient() *Client {
 		log.Fatalf("unable to parse private key: %v", err)
 	}
 
+	hostKeyCallback, err := kh.New(filepath.Join(viper.GetString("home"), ".ssh", "known_hosts"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	sshConfig := &ssh.ClientConfig{
 		User: c.UserName,
 		Auth: []ssh.AuthMethod{
 			ssh.PublicKeys(signer),
 		},
-
-		// verify host public key
-		HostKeyCallback: ssh.FixedHostKey(hostKey),
+		HostKeyCallback: hostKeyCallback,
 
 		Timeout: 10 * time.Second,
 	}
@@ -87,37 +87,4 @@ func (c *Client) GetFile(srcFilePath string, dstFilePath string) {
 		log.Fatal(err)
 	}
 	log.Printf("File %s: %d bytes copied\n", srcFilePath, bytes)
-}
-
-func getHostKey(host string) ssh.PublicKey {
-	// parse OpenSSH known_hosts file
-	// ssh or use ssh-keyscan to get initial key
-	file, err := os.Open(filepath.Join(viper.GetString("home"), ".ssh", "known_hosts"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	var hostKey ssh.PublicKey
-	for scanner.Scan() {
-		fields := strings.Split(scanner.Text(), " ")
-		if len(fields) != 3 {
-			continue
-		}
-		if strings.Contains(fields[0], host) {
-			var err error
-			hostKey, _, _, _, err = ssh.ParseAuthorizedKey(scanner.Bytes())
-			if err != nil {
-				log.Fatalf("error parsing %q: %v", fields[2], err)
-			}
-			break
-		}
-	}
-
-	if hostKey == nil {
-		log.Fatalf("no hostkey found for %s", host)
-	}
-
-	return hostKey
 }
