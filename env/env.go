ae482fc63a49112a9a60026736312dddccd37ffd
Gilles Dubreuil <gilles@redhat.com>
2019-03-30 00:36:38 +0100

    Interface for Clusters

--- env/env.go
+++ env/env.go
@@ -7,10 +7,38 @@ import (
 	"github.com/spf13/viper"
 )
 
-// Info structures the application settings.
+type ClusterCMD interface {
+	addNode(name, file, path string) int
+}
+
+type Clusters map[string]NodeConfig
+
+type NodeConfig struct {
+	FileName string
+	Path     string
+}
+
 type Info struct {
+	Cluster    Clusters        `mapstructure:"cluster"`
 	SFTP       sftpclient.Info `mapstructure:"Source"`
 	OutputPath string          `mapstructure:"outputPath"`
+}
+
+func (cluster Clusters) addNode(name, file, path string) int {
+	x := NodeConfig{}
+	x.setConfig(file, path)
+	cluster[name] = x
+	return 0
+}
+
+func (cluster *Clusters) load(list [][]string) {
+	for _, nc := range list {
+		cluster.addNode(nc[0], nc[1], nc[2])
+	}
+}
+func (config *NodeConfig) setConfig(filename, path string) {
+	config.FileName = filename
+	config.Path = path
 }
 
 // New returns a instance of the application settings.
@@ -20,6 +48,14 @@ func New() *Info {
 	if err := viper.Unmarshal(&info); err != nil {
 		log.Fatalf("unable to parse configuration: %v", err)
 	}
+
+	// TODO: make switch for config data or ask user for data
+	list := make([][]string, 2)
+	list[0] = []string{"master", "master-config.yaml", "/etc/origin/master"}
+	list[1] = []string{"node", "node-config.yaml", "/etc/origin/node"}
+
+	info.Cluster = make(Clusters)
+	info.Cluster.load(list)
 
 	return &info
 }
