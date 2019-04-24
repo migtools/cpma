da5be16adf50629b4c0c0aae5113641be217421e
Alexander Demichev <ademicev@redhat.com>
2019-04-24 14:22:21 +0200

    Add OpenID provider support

---
+++ ocp4/oauth/openid_test.go
@@ -0,0 +1,43 @@
+package oauth
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/fusor/cpma/ocp3"
+)
+
+func TestTranslateMasterConfigOpenID(t *testing.T) {
+	testConfig := ocp3.Config{
+		Masterf: "../../test/oauth/openid-test-master-config.yaml",
+	}
+	masterConfig := testConfig.ParseMaster()
+
+	var expectedCrd OAuthCRD
+	expectedCrd.APIVersion = "config.openshift.io/v1"
+	expectedCrd.Kind = "OAuth"
+	expectedCrd.MetaData.Name = "cluster"
+	expectedCrd.MetaData.NameSpace = "openshift-config"
+
+	var openidIDP identityProviderOpenID
+	openidIDP.Type = "OpenID"
+	openidIDP.Challenge = false
+	openidIDP.Login = true
+	openidIDP.MappingMethod = "claim"
+	openidIDP.Name = "my_openid_connect"
+	openidIDP.OpenID.ClientID = "testid"
+	openidIDP.OpenID.Claims.PreferredUsername = []string{"preferred_username", "email"}
+	openidIDP.OpenID.Claims.Name = []string{"nickname", "given_name", "name"}
+	openidIDP.OpenID.Claims.Email = []string{"custom_email_claim", "email"}
+	openidIDP.OpenID.URLs.Authorize = "https://myidp.example.com/oauth2/authorize"
+	openidIDP.OpenID.URLs.Token = "https://myidp.example.com/oauth2/token"
+	openidIDP.OpenID.ClientSecret.Name = "my_openid_connect-secret"
+
+	expectedCrd.Spec.IdentityProviders = append(expectedCrd.Spec.IdentityProviders, openidIDP)
+
+	resCrd, _, err := Translate(masterConfig.OAuthConfig)
+	require.NoError(t, err)
+	assert.Equal(t, &expectedCrd, resCrd)
+}
