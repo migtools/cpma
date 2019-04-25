48cb28e2803d43a6201f03e87eab0b6924a8a028
Alexander Demichev <ademicev@redhat.com>
2019-04-25 12:32:14 +0200

    Add tests for env package

---
+++ env/env_test.go
@@ -0,0 +1,58 @@
+package env
+
+import (
+	"testing"
+	"time"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitConfig(t *testing.T) {
+	ConfigFile = "../test/test-cpma-config.yml"
+	InitConfig()
+
+	expectedHomeDir, _ := homedir.Dir()
+	actualHomeDir := viperConfig.GetString("home")
+	assert.Equal(t, expectedHomeDir, actualHomeDir)
+
+	expectedConfigFilePath := ConfigFile
+	actualConfigFilePath := viperConfig.ConfigFileUsed()
+	assert.Equal(t, expectedConfigFilePath, actualConfigFilePath)
+}
+
+func TestInitLogger(t *testing.T) {
+	InitLogger()
+	logger := logrus.StandardLogger()
+
+	// Test if info level is set by default
+	expectedLogLevel := logrus.InfoLevel
+	assert.Equal(t, expectedLogLevel, logger.GetLevel())
+
+	// Test formatter
+	expectedFormatter := &logrus.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: time.RFC822,
+	}
+	assert.Equal(t, expectedFormatter, logger.Formatter)
+
+	// Test if hook is set right
+	expectedFileHook, _ := NewLogFileHook(
+		LogFileConfig{
+			Filename: logFile,
+			MaxSize:  5, // MiB
+			Level:    logrus.InfoLevel,
+			Formatter: &logrus.JSONFormatter{
+				PrettyPrint: true,
+			},
+		},
+	)
+	assert.Equal(t, expectedFileHook, logger.Hooks[logrus.InfoLevel][0])
+
+	// Test if debug is set from config
+	viperConfig.Set("debug", true)
+	InitLogger()
+	expectedLogLevel = logrus.DebugLevel
+	assert.Equal(t, expectedLogLevel, logger.GetLevel())
+}
