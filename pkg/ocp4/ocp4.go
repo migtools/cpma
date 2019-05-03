c04ae51891d360c2dcc591e2ca08c6431e92e6e3
Alexander Demichev <ademicev@redhat.com>
2019-05-03 18:29:23 +0300

    Add support for SDN config migration

--- pkg/ocp4/ocp4.go
+++ pkg/ocp4/ocp4.go
@@ -1,9 +1,8 @@
 package ocp4
 
 import (
-	"errors"
-
 	"github.com/fusor/cpma/pkg/ocp4/oauth"
+	"github.com/fusor/cpma/pkg/ocp4/sdn"
 	"github.com/fusor/cpma/pkg/ocp4/secrets"
 	configv1 "github.com/openshift/api/legacyconfig/v1"
 	"github.com/sirupsen/logrus"
@@ -12,10 +11,11 @@ import (
 type Master struct {
 	OAuth   oauth.OAuthCRD
 	Secrets []secrets.Secret
+	Network sdn.NetworkCR
 }
 
 type Node struct {
-  Secrets []secrets.Secret
+	Secrets []secrets.Secret
 }
 
 type Manifests []Manifest
@@ -26,6 +26,7 @@ type Manifest struct {
 	CRD  []byte
 }
 
+// OCP4InstallMsg message about using generated manifests
 const OCP4InstallMsg = `To install OCP4 run the installer as follow in order to add CRDs:
 ' /openshift-install --dir $INSTALL_DIR create install-config'
 './openshift-install --dir $INSTALL_DIR create manifests'
@@ -33,14 +34,25 @@ const OCP4InstallMsg = `To install OCP4 run the installer as follow in order to 
 # Edit them if needed, then run installation:
 './openshift-install --dir $INSTALL_DIR  create cluster'`
 
+const manifestPrefix = "100_CPMA-cluster-"
+
+// Translate translate OCP3 configs
 func (ocp4Master *Master) Translate(ocp3Master configv1.MasterConfig) {
 	if ocp3Master.OAuthConfig != nil {
+		logrus.Debugln("Translating oauth config")
 		oauth, secrets, err := oauth.Translate(ocp3Master.OAuthConfig)
+
 		if err != nil {
 			logrus.WithError(err).Fatalf("Unable to generate OAuth CRD from %+v", ocp3Master.OAuthConfig)
 		}
 		ocp4Master.OAuth = *oauth
 		ocp4Master.Secrets = secrets
+	}
+
+	if &ocp3Master.NetworkConfig != nil {
+		logrus.Debugln("Translating SDN config")
+		networkCR := sdn.Translate(ocp3Master.NetworkConfig)
+		ocp4Master.Network = *networkCR
 	}
 }
 
@@ -48,23 +60,54 @@ func (ocp4Node *Node) Translate(ocp3Node configv1.NodeConfig) {
 }
 
 // GenYAML returns the list of translated CRDs
-func (ocp4Master *Master) GenYAML() ([]Manifest, error) {
+func (ocp4Master *Master) GenYAML() []Manifest {
 	var manifests []Manifest
-	if ocp4Master.OAuth.Kind != "" {
-		manifest := Manifest{Name: "100_CPMA-cluster-config-oauth.yaml", CRD: ocp4Master.OAuth.GenYAML()}
-		manifests = append(manifests, manifest)
 
-		for _, secret := range ocp4Master.Secrets {
-			filename := "100_CPMA-cluster-config-secret-" + secret.Metadata.Name + ".yaml"
-			m := Manifest{Name: filename, CRD: secret.GenYAML()}
-			manifests = append(manifests, m)
-		}
-		return manifests, nil
+	// Generate yaml for oauth config
+	crd := ocp4Master.OAuth.GenYAML()
+	manifests = oauthManifests(ocp4Master.OAuth.Kind, crd, manifests)
+
+	// Generate yaml for oauth secrets
+	manifests = oauthSecrets(ocp4Master.Secrets, manifests)
+
+	// Generate yaml for SDN config
+	networkCR := ocp4Master.Network.GenYAML()
+	manifests = sdnManifest(networkCR, manifests)
+
+	return manifests
+}
+
+func oauthManifests(oauthCRKind string, crd []byte, manifests []Manifest) []Manifest {
+	if oauthCRKind != "" {
+		filename := manifestPrefix + "config-oauth.yaml"
+		manifest := Manifest{Name: filename, CRD: crd}
+		manifests = append(manifests, manifest)
+	} else {
+		logrus.Debugln("Skipping oauth, no manifests found")
 	}
-	return nil, errors.New("No manifests")
+
+	return manifests
+}
+
+func oauthSecrets(ocp4MasterSecrets []secrets.Secret, manifests []Manifest) []Manifest {
+	for _, secret := range ocp4MasterSecrets {
+		filename := manifestPrefix + "config-secret-" + secret.Metadata.Name + ".yaml"
+		m := Manifest{Name: filename, CRD: secret.GenYAML()}
+		manifests = append(manifests, m)
+	}
+
+	return manifests
+}
+
+func sdnManifest(networkCR []byte, manifests []Manifest) []Manifest {
+	filename := manifestPrefix + "config-sdn.yaml"
+	manifest := Manifest{Name: filename, CRD: networkCR}
+	manifests = append(manifests, manifest)
+
+	return manifests
 }
 
 // GenYAML returns the list of translated CRDs
-func (ocp4Node *Node) GenYAML() ([]Manifest, error) {
-	return nil, nil
+func (ocp4Node *Node) GenYAML() []Manifest {
+	return nil
 }
---
+++ pkg/ocp4/sdn/sdn.go
@@ -0,0 +1,106 @@
+package sdn
+
+import (
+	"errors"
+
+	configv1 "github.com/openshift/api/legacyconfig/v1"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
+
+// NetworkCR describes Network CR for OCP4
+type NetworkCR struct {
+	APIVersion string `yaml:"apiVersion"`
+	Kind       string `yaml:"kind"`
+	Spec       struct {
+		ClusterNetworks []ClusterNetwork `yaml:"clusterNetwork"`
+		ServiceNetwork  string           `yaml:"serviceNetwork"`
+		DefaultNetwork  `yaml:"defaultNetwork"`
+	} `yaml:"spec"`
+}
+
+// ClusterNetwork contains CIDR and address size to assign to each node
+type ClusterNetwork struct {
+	CIDR       string `yaml:"cidr"`
+	HostPrefix uint32 `yaml:"hostPrefix"`
+}
+
+// DefaultNetwork containts network type and SDN plugin name
+type DefaultNetwork struct {
+	Type               string `yaml:"type"`
+	OpenshiftSDNConfig struct {
+		Mode string `yaml:"mode"`
+	} `yaml:"openshiftSDNConfig"`
+}
+
+const (
+	apiVersion         = "operator.openshift.io/v1"
+	kind               = "Network"
+	defaultNetworkType = "OpenShiftSDN"
+)
+
+// Translate converts OCPv3 SDN to OCPv4 SDN Custom Resources
+func Translate(networkConfig configv1.MasterNetworkConfig) *NetworkCR {
+	var networkCR NetworkCR
+
+	networkCR.APIVersion = apiVersion
+	networkCR.Kind = kind
+	networkCR.Spec.ServiceNetwork = networkConfig.ServiceNetworkCIDR
+	networkCR.Spec.DefaultNetwork.Type = defaultNetworkType
+
+	// Translate CIDRs and adress size for each node
+	translatedClusterNetworks := translateClusterNetworks(networkConfig.ClusterNetworks)
+	networkCR.Spec.ClusterNetworks = translatedClusterNetworks
+
+	// Translate network plugin name
+	selectedNetworkPlugin, err := selectNetworkPlugin(networkConfig.NetworkPluginName)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	networkCR.Spec.DefaultNetwork.OpenshiftSDNConfig.Mode = selectedNetworkPlugin
+
+	return &networkCR
+}
+
+func translateClusterNetworks(clusterNeworkEntries []configv1.ClusterNetworkEntry) []ClusterNetwork {
+	translatedClusterNetworks := make([]ClusterNetwork, 0)
+
+	for _, networkConfig := range clusterNeworkEntries {
+		var translatedClusterNetwork ClusterNetwork
+
+		translatedClusterNetwork.CIDR = networkConfig.CIDR
+		translatedClusterNetwork.HostPrefix = networkConfig.HostSubnetLength
+
+		translatedClusterNetworks = append(translatedClusterNetworks, translatedClusterNetwork)
+	}
+
+	return translatedClusterNetworks
+}
+
+func selectNetworkPlugin(pluginName string) (string, error) {
+	var selectedName string
+
+	switch pluginName {
+	case "redhat/openshift-ovs-multitenant":
+		selectedName = "Multitenant"
+	case "redhat/openshift-ovs-networkpolicy":
+		selectedName = "NetworkPolicy"
+	case "redhat/openshift-ovs-subnet":
+		selectedName = "Subnet"
+	default:
+		err := errors.New("Network plugin not supported")
+		return "", err
+	}
+
+	return selectedName, nil
+}
+
+// GenYAML returns a YAML of the OAuthCRD
+func (networkCR *NetworkCR) GenYAML() []byte {
+	yamlBytes, err := yaml.Marshal(&networkCR)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	return yamlBytes
+}
