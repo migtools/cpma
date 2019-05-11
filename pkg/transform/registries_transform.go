90786ba3fc73ebc7443a46a11ac1bae983d4123a
Jason Montleon <jmontleo@redhat.com>
2019-05-11 16:53:32 -0400

    Remove registries types file for consistency

--- pkg/transform/registries_transform.go
+++ pkg/transform/registries_transform.go
@@ -6,6 +6,37 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+type Containers struct {
+	Registries map[string]Registries
+}
+
+type Registries struct {
+	List []string `toml:"registries"`
+}
+
+type ImageCR struct {
+	APIVersion string   `yaml:"apiVersion"`
+	Kind       string   `yaml:"kind"`
+	Metadata   Metadata `yaml:"metadata"`
+	Spec       struct {
+		RegistrySources RegistrySources `yaml:"registrySources"`
+	} `yaml:"spec"`
+}
+
+type Metadata struct {
+	Name        string
+	Annotations map[string]string `yaml:"annotations"`
+}
+
+type RegistrySources struct {
+	BlockedRegistries  []string `yaml:"blockedRegistries,omitempty"`
+	InsecureRegistries []string `yaml:"insecureRegistries,omitempty"`
+}
+
+type RegistriesTransform struct {
+	Config *Config
+}
+
 func (c RegistriesTransform) Run(content []byte) (TransformOutput, error) {
 	logrus.Info("RegistriesTransform::Run")
 
