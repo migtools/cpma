f91f2b8cce1be1ab3dcd072f7fb1fcf28114c760
Alexander Demichev <ademicev@redhat.com>
2019-06-10 12:17:56 +0200

    Use logrus hooks to handle log output (#205)
    * Use logrus hooks to handle log output
    
    * Add tests for new log hooks

--- pkg/env/loghooks.go
+++ pkg/env/loghooks.go
@@ -21,6 +21,13 @@ type LogFileHook struct {
 	logWriter io.Writer
 }
 
+// ConsoleWriterHook is a hook that writes logs of specified LogLevels to specified Writer
+type ConsoleWriterHook struct {
+	Writer    io.Writer
+	LogLevels []logrus.Level
+	Formatter logrus.Formatter
+}
+
 // NewLogFileHook instantiates hook and implements Hook interface
 func NewLogFileHook(config LogFileConfig) (logrus.Hook, error) {
 	hook := LogFileHook{
@@ -45,7 +52,32 @@ func (hook *LogFileHook) Fire(entry *logrus.Entry) (err error) {
 	if err != nil {
 		return err
 	}
-	hook.logWriter.Write(b)
+
+	_, err = hook.logWriter.Write(b)
+	if err != nil {
+		return err
+	}
 
 	return nil
+}
+
+// Fire will be called when some logging function is called with current hook
+// It will format log entry to string and write it to appropriate writer
+func (hook *ConsoleWriterHook) Fire(entry *logrus.Entry) error {
+	b, err := hook.Formatter.Format(entry)
+	if err != nil {
+		return err
+	}
+
+	_, err = hook.Writer.Write(b)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+// Levels define on which log levels this hook would trigger
+func (hook *ConsoleWriterHook) Levels() []logrus.Level {
+	return hook.LogLevels
 }
