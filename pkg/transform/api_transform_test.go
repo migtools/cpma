9ad57c8c55fe49836903e52d2adf617e484f4a37
Jason Montleon <jmontleo@redhat.com>
2019-06-11 05:19:35 -0400

    Add API and ETCD report tests (#210)

---
+++ pkg/transform/api_transform_test.go
@@ -0,0 +1,84 @@
+package transform_test
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/fusor/cpma/pkg/decode"
+	"github.com/fusor/cpma/pkg/transform"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func loadAPIExtraction() (transform.APIExtraction, error) {
+	// TODO: Something is broken here in a way that it's causing the translaters
+	// to fail. Need some help with creating test identiy providers in a way
+	// that won't crash the translator
+
+	// Build example identity providers, this is straight copy pasted from
+	// oauth test, IMO this loading of example identity providers should be
+	// some shared test helper
+	file := "testdata/api-test-master-config.yaml" // File copied into transform pkg testdata
+	content, _ := ioutil.ReadFile(file)
+
+	masterConfig, err := decode.MasterConfig(content)
+
+	var extraction transform.APIExtraction
+	extraction.HTTPServingInfo.BindAddress = masterConfig.ServingInfo.BindAddress
+
+	return extraction, err
+}
+
+func TestAPIExtractionTransform(t *testing.T) {
+
+	expectedReport := transform.ReportOutput{
+		Component: "API",
+	}
+
+	expectedReport.Reports = append(expectedReport.Reports,
+		transform.Report{
+			Name:       "API",
+			Kind:       "Port",
+			Supported:  false,
+			Confidence: "red",
+			Comment:    "The API Port for Openshift 4 is 6443 and is non-configurable. Your OCP 3 cluster is currently configured to use port 8443",
+		})
+	testCases := []struct {
+		name            string
+		expectedReports transform.ReportOutput
+	}{
+		{
+			name:            "transform crio extraction",
+			expectedReports: expectedReport,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actualReportsChan := make(chan transform.ReportOutput)
+
+			// Override flush method
+			transform.ReportOutputFlush = func(reports transform.ReportOutput) error {
+				actualReportsChan <- reports
+				return nil
+			}
+
+			testExtraction, err := loadAPIExtraction()
+			require.NoError(t, err)
+
+			go func() {
+				transformOutput, err := testExtraction.Transform()
+				if err != nil {
+					t.Error(err)
+				}
+				for _, output := range transformOutput {
+					output.Flush()
+				}
+			}()
+
+			actualReports := <-actualReportsChan
+			assert.Equal(t, actualReports, tc.expectedReports)
+		})
+
+	}
+}
---
+++ pkg/transform/etcd_transform_test.go
@@ -0,0 +1,95 @@
+package transform_test
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/fusor/cpma/pkg/transform"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/go-ini/ini.v1"
+)
+
+func loadETCDExtraction() (transform.ETCDExtraction, error) {
+	// TODO: Something is broken here in a way that it's causing the translaters
+	// to fail. Need some help with creating test identiy providers in a way
+	// that won't crash the translator
+
+	// Build example identity providers, this is straight copy pasted from
+	// oauth test, IMO this loading of example identity providers should be
+	// some shared test helper
+	file := "testdata/etcd.conf" // File copied into transform pkg testdata
+	content, _ := ioutil.ReadFile(file)
+
+	ETCDConfig, err := ini.Load(content)
+
+	var extraction transform.ETCDExtraction
+	portArray := strings.Split(ETCDConfig.Section("").Key("ETCD_LISTEN_CLIENT_URLS").String(), ":")
+	extraction.ClientPort = portArray[len(portArray)-1]
+	extraction.TLSCipherSuites = ETCDConfig.Section("").Key("ETCD_CIPHER_SUITES").String()
+
+	return extraction, err
+}
+
+func TestETCDExtractionTransform(t *testing.T) {
+
+	expectedReport := transform.ReportOutput{
+		Component: "ETCD",
+	}
+
+	expectedReport.Reports = append(expectedReport.Reports,
+		transform.Report{
+			Name:       "ETCD Client Port",
+			Kind:       "Configuration",
+			Supported:  false,
+			Confidence: "green",
+			Comment:    "The Openshift 4 ETCD Cluster is not configurable and uses port 2379. Your Openshift 3 Cluster is using port 2379",
+		})
+	expectedReport.Reports = append(expectedReport.Reports,
+		transform.Report{
+			Name:       "ETCD TLS Cipher Suites",
+			Kind:       "Configuration",
+			Supported:  false,
+			Confidence: "red",
+			Comment:    "The Openshift 4 ETCD Cluster is not configurable. TLS Cipher Suite configuration was detected, TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256",
+		})
+	testCases := []struct {
+		name            string
+		expectedReports transform.ReportOutput
+	}{
+		{
+			name:            "transform crio extraction",
+			expectedReports: expectedReport,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actualReportsChan := make(chan transform.ReportOutput)
+
+			// Override flush method
+			transform.ReportOutputFlush = func(reports transform.ReportOutput) error {
+				actualReportsChan <- reports
+				return nil
+			}
+
+			testExtraction, err := loadETCDExtraction()
+			require.NoError(t, err)
+
+			go func() {
+				transformOutput, err := testExtraction.Transform()
+				if err != nil {
+					t.Error(err)
+				}
+				for _, output := range transformOutput {
+					output.Flush()
+				}
+			}()
+
+			actualReports := <-actualReportsChan
+			assert.Equal(t, actualReports, tc.expectedReports)
+		})
+
+	}
+}
