b1f5709fb25ac7f4e6dad32c76671f1bab126f63
Alexander Demichev <ademicev@redhat.com>
2019-07-10 10:57:52 +0200

    Cluster report: Refactor and adding tests (#292)
    * Add tests to cluster report

---
+++ pkg/transform/cluster/cluster_test.go
@@ -0,0 +1,318 @@
+package cluster_test
+
+import (
+	"testing"
+
+	"github.com/fusor/cpma/pkg/api"
+	"github.com/fusor/cpma/pkg/transform/cluster"
+	cpmatest "github.com/fusor/cpma/pkg/utils/test"
+	O7tapiroute "github.com/openshift/api/route/v1"
+	"github.com/stretchr/testify/assert"
+	k8sapicore "k8s.io/api/core/v1"
+	k8sapistorage "k8s.io/api/storage/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	k8smachinery "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestReportNodes(t *testing.T) {
+	clusterReport := &cluster.Report{}
+
+	// Create node labels
+	masterNodeLabels := make(map[string]string)
+	masterNodeLabels["node-role.kubernetes.io/master"] = "true"
+
+	masterNodeCapacity := make(k8sapicore.ResourceList)
+	// Add CPU node usage
+	cpuQuantity := resource.Quantity{
+		Format: resource.DecimalSI,
+	}
+	cpuQuantity.Set(int64(2))
+	masterNodeCapacity["cpu"] = cpuQuantity
+
+	// Add node memory usage
+	memoryQuantity := resource.Quantity{
+		Format: resource.BinarySI,
+	}
+	memoryQuantity.Set(int64(2048))
+	masterNodeCapacity["memory"] = memoryQuantity
+
+	// Add pods
+	podsQuantity := resource.Quantity{
+		Format: resource.DecimalSI,
+	}
+	podsQuantity.Set(int64(10))
+	masterNodeCapacity["pods"] = podsQuantity
+
+	// Add resources that are available for scheduling
+	allocatableResources := make(k8sapicore.ResourceList)
+
+	allocatableMemoryQuantity := resource.Quantity{
+		Format: resource.BinarySI,
+	}
+	allocatableMemoryQuantity.Set(int64(2048))
+	masterNodeCapacity["memory"] = allocatableMemoryQuantity
+
+	// Add pod count
+	podList := &k8sapicore.PodList{}
+	podList.Items = make([]k8sapicore.Pod, 0)
+	podList.Items = append(podList.Items, k8sapicore.Pod{
+		Spec: k8sapicore.PodSpec{
+			NodeName: "test-master",
+		},
+	})
+	podList.Items = append(podList.Items, k8sapicore.Pod{
+		Spec: k8sapicore.PodSpec{
+			NodeName: "test-master",
+		},
+	})
+	podList.Items = append(podList.Items, k8sapicore.Pod{
+		Spec: k8sapicore.PodSpec{
+			NodeName: "test-master",
+		},
+	})
+	podList.Items = append(podList.Items, k8sapicore.Pod{
+		Spec: k8sapicore.PodSpec{
+			NodeName: "not-this-node",
+		},
+	})
+
+	namespaceList := make([]api.NamespaceResources, 0)
+	namespaceList = append(namespaceList, api.NamespaceResources{
+		PodList: podList,
+	})
+
+	// Init fake nodes
+	nodes := make([]k8sapicore.Node, 0)
+	nodes = append(nodes, k8sapicore.Node{
+		ObjectMeta: k8smachinery.ObjectMeta{
+			Name:   "test-master",
+			Labels: masterNodeLabels,
+		},
+		Status: k8sapicore.NodeStatus{
+			Capacity:    masterNodeCapacity,
+			Allocatable: allocatableResources,
+		},
+	})
+
+	testCases := []struct {
+		name                string
+		actualClusterReport *cluster.Report
+	}{
+		{
+			name:                "generate node report",
+			actualClusterReport: clusterReport,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.actualClusterReport.ReportNodes(api.Resources{
+				NodeList:      cpmatest.CreateTestNodeList(),
+				NamespaceList: namespaceList,
+			})
+
+			assert.Equal(t, "test-master", tc.actualClusterReport.Nodes[0].Name)
+			assert.Equal(t, true, tc.actualClusterReport.Nodes[0].MasterNode)
+			assert.Equal(t, int64(2), tc.actualClusterReport.Nodes[0].Resources.CPU.Value())
+			assert.Equal(t, int64(2048), tc.actualClusterReport.Nodes[0].Resources.MemoryConsumed.Value())
+			assert.Equal(t, int64(2048), tc.actualClusterReport.Nodes[0].Resources.MemoryCapacity.Value())
+			assert.Equal(t, int64(3), tc.actualClusterReport.Nodes[0].Resources.RunningPods.Value())
+			assert.Equal(t, int64(10), tc.actualClusterReport.Nodes[0].Resources.PodCapacity.Value())
+		})
+	}
+}
+
+func TestReportPods(t *testing.T) {
+	expectedPodRepors := make([]cluster.PodReport, 0)
+	expectedPodRepors = append(expectedPodRepors, cluster.PodReport{Name: "test-pod1"})
+	expectedPodRepors = append(expectedPodRepors, cluster.PodReport{Name: "test-pod2"})
+
+	testCases := []struct {
+		name              string
+		inputPodList      *k8sapicore.PodList
+		expectedPodRepors []cluster.PodReport
+	}{
+		{
+			name:              "generate pod report",
+			inputPodList:      cpmatest.CreateTestPodList(),
+			expectedPodRepors: expectedPodRepors,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			reportedNamespace := &cluster.NamespaceReport{}
+			cluster.ReportPods(reportedNamespace, tc.inputPodList)
+			assert.Equal(t, tc.expectedPodRepors, reportedNamespace.Pods)
+		})
+	}
+}
+
+func TestReportNamespaceResources(t *testing.T) {
+	expectedCPU := &resource.Quantity{
+		Format: resource.DecimalSI,
+	}
+	expectedCPU.Set(int64(2))
+	expectedMemory := &resource.Quantity{
+		Format: resource.BinarySI,
+	}
+	expectedMemory.Set(int64(2))
+
+	expectedResources := &cluster.ContainerResourcesReport{
+		ContainerCount: 2,
+		CPUTotal:       expectedCPU,
+		MemoryTotal:    expectedMemory,
+	}
+
+	testCases := []struct {
+		name              string
+		inputPodList      *k8sapicore.PodList
+		expectedResources cluster.ContainerResourcesReport
+	}{
+		{
+			name:              "generate resource report",
+			inputPodList:      cpmatest.CreateTestPodResourceList(),
+			expectedResources: *expectedResources,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			reportedNamespace := &cluster.NamespaceReport{}
+			cluster.ReportResources(reportedNamespace, tc.inputPodList)
+			assert.Equal(t, tc.expectedResources, reportedNamespace.Resources)
+		})
+	}
+}
+
+func TestReportRoutes(t *testing.T) {
+	alternateBackends := make([]O7tapiroute.RouteTargetReference, 0)
+	alternateBackends = append(alternateBackends, O7tapiroute.RouteTargetReference{
+		Kind: "testkind",
+		Name: "testname",
+	})
+
+	to := O7tapiroute.RouteTargetReference{
+		Kind: "testkindTo",
+		Name: "testTo",
+	}
+
+	tls := &O7tapiroute.TLSConfig{
+		Termination: O7tapiroute.TLSTerminationEdge,
+	}
+
+	expectedRouteReport := make([]cluster.RouteReport, 0)
+	expectedRouteReport = append(expectedRouteReport, cluster.RouteReport{
+		Name:              "route1",
+		Host:              "testhost",
+		Path:              "testpath",
+		AlternateBackends: alternateBackends,
+		TLS:               tls,
+		To:                to,
+		WildcardPolicy:    "None",
+	})
+
+	testCases := []struct {
+		name                string
+		inputRouteList      *O7tapiroute.RouteList
+		expectedRouteReport []cluster.RouteReport
+	}{
+		{
+			name:                "generate route report",
+			inputRouteList:      cpmatest.CreateTestRouteList(),
+			expectedRouteReport: expectedRouteReport,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			reportedNamespace := &cluster.NamespaceReport{}
+			cluster.ReportRoutes(reportedNamespace, tc.inputRouteList)
+			assert.Equal(t, tc.expectedRouteReport, reportedNamespace.Routes)
+		})
+	}
+}
+
+func TestPVReport(t *testing.T) {
+	resources := make(k8sapicore.ResourceList)
+	cpu := resource.Quantity{
+		Format: resource.DecimalSI,
+	}
+	cpu.Set(int64(1))
+	resources["cpu"] = cpu
+
+	memory := resource.Quantity{
+		Format: resource.BinarySI,
+	}
+	memory.Set(int64(1))
+	resources["memory"] = memory
+
+	driver := k8sapicore.PersistentVolumeSource{
+		NFS: &k8sapicore.NFSVolumeSource{
+			Server: "example.com",
+		},
+	}
+
+	expectedPVReport := make([]cluster.PVReport, 0)
+	expectedPVReport = append(expectedPVReport, cluster.PVReport{
+		Name:         "testpv",
+		Driver:       driver,
+		StorageClass: "testclass",
+		Capacity:     resources,
+		Phase:        k8sapicore.VolumePending,
+	})
+
+	testCases := []struct {
+		name             string
+		inputPVList      *k8sapicore.PersistentVolumeList
+		expectedPVReport []cluster.PVReport
+	}{
+		{
+			name:             "generate route report",
+			inputPVList:      cpmatest.CreateTestPVList(),
+			expectedPVReport: expectedPVReport,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			clusterReport := &cluster.Report{}
+			clusterReport.ReportPVs(api.Resources{
+				PersistentVolumeList: tc.inputPVList,
+			})
+
+			assert.Equal(t, tc.expectedPVReport, clusterReport.PVs)
+		})
+	}
+}
+
+func TestStorageClassReport(t *testing.T) {
+	expectedStorageClassReport := make([]cluster.StorageClassReport, 0)
+	expectedStorageClassReport = append(expectedStorageClassReport, cluster.StorageClassReport{
+		Name:        "testclass",
+		Provisioner: "testprovisioner",
+	})
+
+	testCases := []struct {
+		name                       string
+		inputStorageClassList      *k8sapistorage.StorageClassList
+		expectedStorageClassReport []cluster.StorageClassReport
+	}{
+		{
+			name:                       "generate storage class report",
+			inputStorageClassList:      cpmatest.CreateStorageClassList(),
+			expectedStorageClassReport: expectedStorageClassReport,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			clusterReport := &cluster.Report{}
+			clusterReport.ReportStorageClasses(api.Resources{
+				StorageClassList: tc.inputStorageClassList,
+			})
+
+			assert.Equal(t, tc.expectedStorageClassReport, clusterReport.StorageClasses)
+		})
+	}
+}
--- pkg/transform/cluster_report_transform.go
+++ pkg/transform/cluster_report_transform.go
@@ -2,11 +2,8 @@ package transform
 
 import (
 	"github.com/fusor/cpma/pkg/api"
-	O7tapiroute "github.com/openshift/api/route/v1"
+	"github.com/fusor/cpma/pkg/transform/cluster"
 	"github.com/sirupsen/logrus"
-
-	k8sapicore "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 )
 
 // ClusterReportName is the cluster report name
@@ -25,7 +22,7 @@ type ClusterTransform struct {
 func (e ClusterReportExtraction) Transform() ([]Output, error) {
 	logrus.Info("ClusterTransform::Transform")
 
-	clusterReport := genClusterReport(api.Resources{
+	clusterReport := cluster.GenClusterReport(api.Resources{
 		PersistentVolumeList: e.PersistentVolumeList,
 		StorageClassList:     e.StorageClassList,
 		NamespaceList:        e.NamespaceList,
@@ -38,160 +35,6 @@ func (e ClusterReportExtraction) Transform() ([]Output, error) {
 
 	outputs := []Output{output}
 	return outputs, nil
-}
-
-// genClusterReport inserts report values into structures for json output
-func genClusterReport(apiResources api.Resources) (clusterReport ClusterReport) {
-	clusterReport.reportNodes(apiResources)
-	clusterReport.reportNamespaces(apiResources)
-	clusterReport.reportPVs(apiResources)
-	clusterReport.reportStorageClasses(apiResources)
-	return
-}
-
-// reportNodes fills in information about nodes
-func (clusterReport *ClusterReport) reportNodes(apiResources api.Resources) {
-	logrus.Debug("ClusterReport::ReportNodes")
-
-	for _, node := range apiResources.NodeList.Items {
-		nodeReport := &NodeReport{
-			Name: node.ObjectMeta.Name,
-		}
-
-		isMaster, ok := node.ObjectMeta.Labels["node-role.kubernetes.io/master"]
-		nodeReport.MasterNode = ok && isMaster == "true"
-
-		reportNodeResources(nodeReport, node.Status, apiResources)
-		clusterReport.Nodes = append(clusterReport.Nodes, *nodeReport)
-	}
-}
-
-// reportResources parse and insert info about consumed resources
-func reportNodeResources(repotedNode *NodeReport, nodeStatus k8sapicore.NodeStatus, apiResources api.Resources) {
-	repotedNode.Resources.CPU = nodeStatus.Capacity.Cpu()
-
-	repotedNode.Resources.MemoryCapacity = nodeStatus.Capacity.Memory()
-
-	memConsumed := new(resource.Quantity)
-	memCapacity, _ := nodeStatus.Capacity.Memory().AsInt64()
-	memAllocatable, _ := nodeStatus.Allocatable.Memory().AsInt64()
-	memConsumed.Set(memCapacity - memAllocatable)
-	memConsumed.Format = resource.BinarySI
-	repotedNode.Resources.MemoryConsumed = memConsumed
-
-	var runningPodsCount int64
-	for _, resources := range apiResources.NamespaceList {
-		for _, pod := range resources.PodList.Items {
-			if pod.Spec.NodeName == repotedNode.Name {
-				runningPodsCount++
-			}
-		}
-	}
-	podsRunning := new(resource.Quantity)
-	podsRunning.Set(runningPodsCount)
-	podsRunning.Format = resource.DecimalSI
-	repotedNode.Resources.RunningPods = podsRunning
-
-	repotedNode.Resources.PodCapacity = nodeStatus.Capacity.Pods()
-}
-
-// reportNamespaces fills in information about Namespaces
-func (clusterReport *ClusterReport) reportNamespaces(apiResources api.Resources) {
-	logrus.Debug("ClusterReport::ReportNamespaces")
-
-	for _, resources := range apiResources.NamespaceList {
-		reportedNamespace := NamespaceReport{Name: resources.NamespaceName}
-
-		reportPods(&reportedNamespace, resources.PodList)
-		reportResources(&reportedNamespace, resources.PodList)
-		reportRoutes(&reportedNamespace, resources.RouteList)
-		clusterReport.Namespaces = append(clusterReport.Namespaces, reportedNamespace)
-	}
-}
-
-// reportPods creates info about cluster pods
-func reportPods(reportedNamespace *NamespaceReport, podList *k8sapicore.PodList) {
-	for _, pod := range podList.Items {
-		reportedPod := &PodReport{Name: pod.Name}
-		reportedNamespace.Pods = append(reportedNamespace.Pods, *reportedPod)
-
-		// Update namespace touch timestamp
-		if pod.ObjectMeta.CreationTimestamp.Time.Unix() > reportedNamespace.LatestChange.Time.Unix() {
-			reportedNamespace.LatestChange = pod.ObjectMeta.CreationTimestamp
-		}
-	}
-}
-
-func reportResources(reportedNamespace *NamespaceReport, podList *k8sapicore.PodList) {
-	resources := ContainerResourcesReport{
-		CPUTotal:    &resource.Quantity{Format: resource.DecimalSI},
-		MemoryTotal: &resource.Quantity{Format: resource.BinarySI},
-	}
-	reportedNamespace.Resources = resources
-
-	for _, pod := range podList.Items {
-		reportContainerResources(reportedNamespace, &pod)
-	}
-}
-
-func reportRoutes(reportedNamespace *NamespaceReport, routeList *O7tapiroute.RouteList) {
-	for _, route := range routeList.Items {
-		reportedRoute := &RouteReport{
-			Name:              route.Name,
-			AlternateBackends: route.Spec.AlternateBackends,
-			Host:              route.Spec.Host,
-			Path:              route.Spec.Path,
-			To:                route.Spec.To,
-			TLS:               route.Spec.TLS,
-			WildcardPolicy:    route.Spec.WildcardPolicy,
-		}
-
-		reportedNamespace.Routes = append(reportedNamespace.Routes, *reportedRoute)
-	}
-}
-func reportContainerResources(reportedNamespace *NamespaceReport, pod *k8sapicore.Pod) {
-	cpuTotal := reportedNamespace.Resources.CPUTotal.Value()
-	memoryTotal := reportedNamespace.Resources.MemoryTotal.Value()
-
-	for _, container := range pod.Spec.Containers {
-		cpuTotal += container.Resources.Requests.Cpu().Value()
-		memoryTotal += container.Resources.Requests.Memory().Value()
-	}
-	reportedNamespace.Resources.CPUTotal.Set(cpuTotal)
-	reportedNamespace.Resources.MemoryTotal.Set(memoryTotal)
-	reportedNamespace.Resources.ContainerCount += len(pod.Spec.Containers)
-}
-
-func (clusterReport *ClusterReport) reportPVs(apiResources api.Resources) {
-	logrus.Debug("ClusterReport::ReportPVs")
-	pvList := apiResources.PersistentVolumeList
-
-	// Go through all PV and save required information to report
-	for _, pv := range pvList.Items {
-		reportedPV := &PVReport{
-			Name:         pv.Name,
-			Driver:       pv.Spec.PersistentVolumeSource,
-			StorageClass: pv.Spec.StorageClassName,
-			Capacity:     pv.Spec.Capacity,
-			Phase:        pv.Status.Phase,
-		}
-
-		clusterReport.PVs = append(clusterReport.PVs, *reportedPV)
-	}
-}
-
-func (clusterReport *ClusterReport) reportStorageClasses(apiResources api.Resources) {
-	logrus.Debug("ClusterReport::ReportStorageClasses")
-	// Go through all storage classes and save required information to report
-	storageClassList := apiResources.StorageClassList
-	for _, storageClass := range storageClassList.Items {
-		reportedStorageClass := &StorageClassReport{
-			Name:        storageClass.Name,
-			Provisioner: storageClass.Provisioner,
-		}
-
-		clusterReport.StorageClasses = append(clusterReport.StorageClasses, *reportedStorageClass)
-	}
 }
 
 // Validate no need to validate it, data is exctracted from API
