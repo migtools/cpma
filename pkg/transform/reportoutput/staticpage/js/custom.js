400f61bd56082bdf0adf33345c446ae01859ecf4
Alexander Demichev <ademicev@redhat.com>
2019-08-26 15:35:33 +0200

    Deduplicate comment

--- pkg/transform/reportoutput/staticpage/js/custom.js
+++ pkg/transform/reportoutput/staticpage/js/custom.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
   animateDropdownArrowIcon();
   fillEmptyTableData();
   sortDateRows();
@@ -10,20 +10,20 @@ var iconUp = `
     <path fill="currentColor" d="M177 159.7l136 136c9.4 9.4 9.4 24.6 0 33.9l-22.6 22.6c-9.4 9.4-24.6 9.4-33.9 0L160 255.9l-96.4 96.4c-9.4 9.4-24.6 9.4-33.9 0L7 329.7c-9.4-9.4-9.4-24.6 0-33.9l136-136c9.4-9.5 24.6-9.5 34-.1z"></path>
   </svg>`;
 
-var iconRight =
-  `<svg width="8" height="16" class="angle-right" viewBox="0 0 256 512">
+var iconRight = `
+      <svg width="8" height="16" class="angle-right" viewBox="0 0 256 512">
         <path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path>
        </svg>`;
 
-var iconDown =
-  `<svg width="10" height="16" class="angle-down" viewBox="0 0 320 512">
+var iconDown = `
+      <svg width="10" height="16" class="angle-down" viewBox="0 0 320 512">
         <path fill="currentColor" d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"></path>
        </svg>`;
 
 function animateDropdownArrowIcon() {
   $('.report-btn').append(iconRight);
 
-  $('.report-btn').click(function () {
+  $('.report-btn').click(function() {
     var expanded = $.parseJSON($(this).attr('aria-expanded'));
     if (!expanded) {
       $(this).children('svg').replaceWith(iconDown);
@@ -34,51 +34,53 @@ function animateDropdownArrowIcon() {
 }
 
 function fillEmptyTableData() {
-  $("td").each(function () {
+  $('td').each(function() {
     if (!$(this).text().trim().length) {
-      $(this).text("None");
+      $(this).text('None');
     }
   });
 }
 
 function sortDateRows() {
-  commonTableSort('.date-th',
-    function (a, b) {
-      var aDate = $(a).children(".date-td").text();
-      var bDate = $(b).children(".date-td").text();
+  commonTableSort(
+      '.date-th',
+      function(a, b) {
+        var aDate = $(a).children('.date-td').text();
+        var bDate = $(b).children('.date-td').text();
 
-      return new Date(aDate) - new Date(bDate);
-    },
-    function (a, b) {
-      var aDate = $(a).children(".date-td").text();
-      var bDate = $(b).children(".date-td").text();
+        return new Date(aDate) - new Date(bDate);
+      },
+      function(a, b) {
+        var aDate = $(a).children('.date-td').text();
+        var bDate = $(b).children('.date-td').text();
 
-      return new Date(bDate) - new Date(aDate);
-    });
+        return new Date(bDate) - new Date(aDate);
+      });
 }
 
 function sortStringRows() {
-  commonTableSort('.string-th',
-    function (a, b) {
-      var aStr = $(a).children('.string-td').text();
-      var bStr = $(b).children('.string-td').text();
+  commonTableSort(
+      '.string-th',
+      function(a, b) {
+        var aStr = $(a).children('.string-td').text();
+        var bStr = $(b).children('.string-td').text();
 
-      return aStr.localeCompare(bStr);
-    },
-    function (a, b) {
-      var aStr = $(a).children('.string-td').text();
-      var bStr = $(b).children('.string-td').text();
+        return aStr.localeCompare(bStr);
+      },
+      function(a, b) {
+        var aStr = $(a).children('.string-td').text();
+        var bStr = $(b).children('.string-td').text();
 
-      return bStr.localeCompare(aStr);
-    });
+        return bStr.localeCompare(aStr);
+      });
 }
 
 function commonTableSort(th, ascSortFunc, descSortFunc) {
   // set initial arrow icon
   $(th).append(iconDown);
-  $(th).children('svg').css("margin-left", "10px");
+  $(th).children('svg').css('margin-left', '10px');
 
-  $(th).click(function () {
+  $(th).click(function() {
     var parentTable = $(this).parents()[2];
     var parentTableHead = $(parentTable).children('thead');
     var tableHeaders = $($(parentTableHead).children('tr')).children('th');
@@ -92,13 +94,12 @@ function commonTableSort(th, ascSortFunc, descSortFunc) {
       tableRows.sort(ascSortFunc);
       // set right icon on click
       $(this).children('svg').replaceWith(iconDown);
-      $(this).children('svg').css("margin-left", "10px");
+      $(this).children('svg').css('margin-left', '10px');
       $(this).attr('sorted', true);
     } else {
       tableRows.sort(descSortFunc);
-      // set right icon on click
       $(this).children('svg').replaceWith(iconUp);
-      $(this).children('svg').css("margin-left", "10px");
+      $(this).children('svg').css('margin-left', '10px');
       $(this).attr('sorted', false);
     }
 
@@ -115,7 +116,7 @@ function commonTableSort(th, ascSortFunc, descSortFunc) {
 function organizeIndexes(tableRows) {
   var i = 1;
   tableRows.forEach(x => {
-    $(x).children("th").text(i.toString());
+    $(x).children('th').text(i.toString());
     i++;
   });
 }
