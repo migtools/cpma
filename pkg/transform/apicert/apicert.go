db0b9e7d3d4960e254f5de452b31c88070ac6036
Gilles Dubreuil <gilles@redhat.com>
2019-09-02 23:09:43 +0200

    Get Certificate signer CN

--- pkg/transform/apicert/apicert.go
+++ pkg/transform/apicert/apicert.go
@@ -1,12 +1,17 @@
 package apicert
 
 import (
+	"crypto/x509"
+	"encoding/pem"
+	"log"
 	"path/filepath"
+	"strings"
 
 	"github.com/fusor/cpma/pkg/io"
 	"github.com/fusor/cpma/pkg/transform/secrets"
 	legacyconfigv1 "github.com/openshift/api/legacyconfig/v1"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 
 	corev1 "k8s.io/api/core/v1"
 )
@@ -34,6 +39,10 @@ func Translate(servingInfo legacyconfigv1.ServingInfo) (*corev1.Secret, error) {
 		return nil, err
 	}
 
+	if strings.Contains(certSigner(certContent), "openshift-signer@") {
+		logrus.Info("APITransform:API certficate is openshift signed")
+	}
+
 	path = defaultCertPath
 	dir, file = filepath.Split(servingInfo.KeyFile)
 	if dir != "" {
@@ -51,4 +60,17 @@ func Translate(servingInfo legacyconfigv1.ServingInfo) (*corev1.Secret, error) {
 	}
 
 	return tlsSecret, nil
+}
+
+func certSigner(certContent []byte) string {
+	block, _ := pem.Decode(certContent)
+	if block == nil || block.Type != "CERTIFICATE" {
+		return ""
+	}
+
+	certif, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		log.Fatalf("Can't read certif: %s", err)
+	}
+	return certif.Issuer.CommonName
 }
