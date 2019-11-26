05362ea2e9379d1daa95f713cdb03bcb23a85116
Gilles Dubreuil <gilles@redhat.com>
2019-11-26 14:26:32 +0100

    Api init refactor (#432)
    * refactor singleton API client initialization
    
    * Rely on k8s and openshift libs for init or die
    
    (cherry picked from commit 8a65ff7ed39ef74e55681aa3467e6c76b2f132b4)

--- pkg/api/k8s.go
+++ pkg/api/k8s.go
@@ -5,14 +5,7 @@ import (
 	"k8s.io/client-go/rest"
 )
 
-// InitK8SOrDie init k8s client or panic
-func InitK8SOrDie(config *rest.Config) *kubernetes.Clientset {
-	once.K8S.Do(func() {
-		k8sClient, err := kubernetes.NewForConfig(config)
-		if err != nil {
-			panic("Error in creating K8S API client")
-		}
-		instances.K8S = k8sClient
-	})
-	return instances.K8S
+// NewK8SOrDie init k8s client or panic
+func NewK8SOrDie(config *rest.Config) *kubernetes.Clientset {
+	return kubernetes.NewForConfigOrDie(config)
 }
