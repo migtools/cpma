35d037ea729d3805130279e858366e520a677bda
Gilles Dubreuil <gilles@redhat.com>
2019-11-27 17:45:57 +0100

    Discovery is included in k8s client (#435)

--- pkg/api/k8s.go
+++ pkg/api/k8s.go
@@ -1,15 +1,9 @@
 package api
 
 import (
-	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
-
-// NewDiscoveryOrDie init k8s client or panic
-func NewDiscoveryOrDie(config *rest.Config) *discovery.DiscoveryClient {
-	return discovery.NewDiscoveryClientForConfigOrDie(config)
-}
 
 // NewK8SOrDie init k8s client or panic
 func NewK8SOrDie(config *rest.Config) *kubernetes.Clientset {
